013bd0b0abb64757056539ae3182033b226a0cc9
123-code <naranjojose256@gmail.com>
2022-07-25 23:26:27 -0500

    Added File

---
+++ env/Algorithms/LinearRegression.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+class LinearRegression:
+
+    def __init__(self):
+        pass
+
+    
+    def forward():
+        pass
+
+
+    def backward():
+        pass
+
+
+
+
+
+
+
+
+
+
+
+    
